3bcd06aa9ae827d5d74554ad4d5311248f12f22b
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2021-10-09 23:50:57 +0300

    feat(lib): parallelize the reader and parser

--- kparams-core/src/error.rs
+++ kparams-core/src/error.rs
@@ -6,10 +6,15 @@ pub enum Error {
     /// Error that might occur during I/O operations.
     #[error("IO error: `{0}`")]
     IoError(#[from] std::io::Error),
+    /// Error that might occur whenever a lock is acquired.
+    #[error("Thread lock error: `{0}`")]
+    ThreadLockError(String),
     /// Error that might occur while parsing documents.
     #[error("parse error: `{0}`")]
     ParseError(String),
 }
+
+unsafe impl Send for Error {}
 
 /// Type alias for the standard [`Result`] type.
 pub type Result<T> = core::result::Result<T, Error>;
--- kparams/src/lib.rs
+++ kparams/src/lib.rs
@@ -2,24 +2,36 @@
 
 #![warn(missing_docs, clippy::unwrap_used)]
 
-use kparams_core::error::Result;
+use kparams_core::error::{Error, Result};
 use kparams_core::kernel::SysctlSection;
 use kparams_core::reader;
 use kparams_parser::parser::RstParser;
+use rayon::prelude::*;
 use std::path::PathBuf;
+use std::sync::Mutex;
 
 /// Runs `kparams`.
 pub fn run() -> Result<()> {
     let kernel_docs = PathBuf::from("/usr/share/doc/linux");
     let sysctl_docs = kernel_docs.join("admin-guide").join("sysctl");
 
-    let mut kernel_parameters = Vec::new();
-    for section in SysctlSection::variants().iter() {
-        let docs = reader::read_to_string(&sysctl_docs.join(section.as_file()))?;
-        kernel_parameters.extend(RstParser::parse_docs(&docs, *section)?);
-    }
+    let kernel_parameters = Mutex::new(Vec::new());
+    SysctlSection::variants().par_iter().try_for_each(|s| {
+        let mut kernel_parameters = kernel_parameters
+            .lock()
+            .map_err(|e| Error::ThreadLockError(e.to_string()))?;
+        let mut parse = |section: SysctlSection| -> Result<()> {
+            let docs = reader::read_to_string(&sysctl_docs.join(section.as_file()))?;
+            Ok(kernel_parameters.extend(RstParser::parse_docs(&docs, section)?))
+        };
+        parse(*s)
+    })?;
 
-    for param in kernel_parameters {
+    for param in kernel_parameters
+        .lock()
+        .map_err(|e| Error::ThreadLockError(e.to_string()))?
+        .iter()
+    {
         println!("## {}::{}\n", param.section, param.name);
         println!("{}\n", param.description);
     }
