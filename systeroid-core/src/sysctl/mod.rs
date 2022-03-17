f2145cb9ca96300bb33472975a47482077ac46d6
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-03-18 02:16:10 +0300

    feat(app): support `KERNEL_DOCS` variable for specifying the documentation path

--- systeroid-core/src/sysctl/mod.rs
+++ systeroid-core/src/sysctl/mod.rs
@@ -29,3 +29,6 @@ pub const SYSTEM_PRELOAD: &[&str] = &[
 /// Deprecated parameters to skip while listing.
 /// <https://bugzilla.redhat.com/show_bug.cgi?id=152435>
 pub const DEPRECATED_PARAMS: &[&str] = &["base_reachable_time", "retrans_time"];
+
+/// Environment variable for setting the path of the Linux kernel documentation.
+pub const KERNEL_DOCS_ENV: &str = "KERNEL_DOCS";
