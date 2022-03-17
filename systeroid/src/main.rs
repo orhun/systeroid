fa4be8228f0d6624062c2760456850429c7b77f9
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-03-18 01:00:58 +0300

    feat(app): allow passing -D parameter to tui while launching

--- systeroid/src/main.rs
+++ systeroid/src/main.rs
@@ -7,7 +7,11 @@ fn main() {
     if let Some(args) = Args::parse(env::args().collect()) {
         if args.show_tui {
             let bin = format!("{}-tui", env!("CARGO_PKG_NAME"));
-            match Command::new(&bin).spawn().map(|mut child| child.wait()) {
+            let mut command = Command::new(&bin);
+            if let Some(kernel_docs) = args.kernel_docs {
+                command.arg("--docs").arg(kernel_docs);
+            }
+            match command.spawn().map(|mut child| child.wait()) {
                 Ok(_) => process::exit(0),
                 Err(e) => {
                     eprintln!("Cannot run `{}` ({})", bin, e);
