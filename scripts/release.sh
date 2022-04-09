7474c89cec8aa3e7565a69a64f7d3e42a6613b74
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-04-09 12:20:31 +0300

    style(script): update indentation in the release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -21,18 +21,18 @@ changelog=$(git diff -U0 "$changelog_file" | grep '^[+][^+]' | sed 's/^[+]//;s/^
 [ -z "$changelog" ] && bail "$changelog_file is not updated"
 
 sed "s/^version = \".*\" $version_suffix$/version = \"${1#v}\" $version_suffix/g" \
-	-i -- */Cargo.toml
+    -i -- */Cargo.toml
 
 gawk -i inplace \
-	-v date="\"$(date +%Y-%m-%d)\"" \
-	'/\.TH\s.*+"8".*"System Administration"/{ $4 = date } 1' man*/*
+    -v date="\"$(date +%Y-%m-%d)\"" \
+    '/\.TH\s.*+"8".*"System Administration"/{ $4 = date } 1' man*/*
 
 git add -A
 git commit -m "$commit_prefix$1"
 git show
 
 git -c user.name="$tagger_name" \
-	-c user.email="$tagger_email" \
-	-c user.signingkey="$signing_key" \
-	tag -s -a "$1" -m "$tag_prefix$1" -m "$changelog"
+    -c user.email="$tagger_email" \
+    -c user.signingkey="$signing_key" \
+    tag -s -a "$1" -m "$tag_prefix$1" -m "$changelog"
 git tag -v "$1"
