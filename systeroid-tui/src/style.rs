56c9281f3ae5a3bc31023061e2fe57153b175c64
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-06-17 21:25:13 +0300

    chore(deps): bump tui dependencies

--- systeroid-tui/src/style.rs
+++ systeroid-tui/src/style.rs
@@ -1,95 +1,47 @@
 use crate::error::Result;
-use colorsys::{ParseError, Rgb};
-use std::result::Result as StdResult;
 use std::str::FromStr;
 use tui::style::{Color as TuiColor, Style};
 
 /// Color configuration.
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Colors {
     /// Background color.
-    bg: Color,
+    bg: TuiColor,
     /// Foreground color.
-    fg: Color,
+    fg: TuiColor,
+}
+
+impl Default for Colors {
+    fn default() -> Self {
+        Colors {
+            bg: TuiColor::Reset,
+            fg: TuiColor::Reset,
+        }
+    }
 }
 
 impl Colors {
     /// Constructs a new instance.
     pub fn new(background: &str, foreground: &str) -> Result<Self> {
         Ok(Self {
-            bg: Color::from_str(background)?,
-            fg: Color::from_str(foreground)?,
+            bg: TuiColor::from_str(background)?,
+            fg: TuiColor::from_str(foreground)?,
         })
     }
 
     /// Returns the background/foreground colors with default style.
     pub fn get_style(&self) -> Style {
-        Style::default().bg(self.fg.get()).fg(self.bg.get())
+        Style::default().bg(self.fg).fg(self.bg)
     }
 
     /// Returns the background color with default style.
     pub fn get_bg_style(&self) -> Style {
-        Style::default().bg(self.bg.get())
+        Style::default().bg(self.bg)
     }
 
     /// Returns the foreground color with default style.
     pub fn get_fg_style(&self) -> Style {
-        Style::default().fg(self.fg.get())
-    }
-}
-
-/// Wrapper for widget colors.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct Color {
-    /// Inner type.
-    inner: TuiColor,
-}
-
-impl Default for Color {
-    fn default() -> Self {
-        Self {
-            inner: TuiColor::Reset,
-        }
-    }
-}
-
-impl Color {
-    /// Returns the underlying [`Color`] type.
-    ///
-    /// [`Color`]: tui::style::Color
-    pub fn get(self) -> TuiColor {
-        self.inner
-    }
-}
-
-impl FromStr for Color {
-    type Err = ParseError;
-    fn from_str(s: &str) -> StdResult<Self, Self::Err> {
-        Ok(Self {
-            inner: match s.to_lowercase().as_ref() {
-                "reset" => TuiColor::Reset,
-                "black" => TuiColor::Black,
-                "red" => TuiColor::Red,
-                "green" => TuiColor::Green,
-                "yellow" => TuiColor::Yellow,
-                "blue" => TuiColor::Blue,
-                "magenta" => TuiColor::Magenta,
-                "cyan" => TuiColor::Cyan,
-                "gray" => TuiColor::Gray,
-                "darkgray" | "dark gray" => TuiColor::DarkGray,
-                "lightred" | "light red" => TuiColor::LightRed,
-                "lightgreen" | "light green" => TuiColor::LightGreen,
-                "lightyellow" | "light yellow" => TuiColor::LightYellow,
-                "lightblue" | "light blue" => TuiColor::LightBlue,
-                "lightmagenta" | "light magenta" => TuiColor::LightMagenta,
-                "lightcyan" | "light cyan" => TuiColor::LightCyan,
-                "white" => TuiColor::White,
-                _ => {
-                    let rgb = Rgb::from_hex_str(&format!("#{s}"))?;
-                    TuiColor::Rgb(rgb.red() as u8, rgb.green() as u8, rgb.blue() as u8)
-                }
-            },
-        })
+        Style::default().fg(self.fg)
     }
 }
 
@@ -97,30 +49,11 @@ impl FromStr for Color {
 mod tests {
     use super::*;
     #[test]
-    fn test_color() -> Result<()> {
-        assert_eq!(TuiColor::Reset, Color::default().get());
-        assert_eq!(TuiColor::Gray, Color::from_str("gray")?.get());
-        assert_eq!(TuiColor::Black, Color::from_str("black")?.get());
-        assert_eq!(TuiColor::Green, Color::from_str("green")?.get());
-        assert_eq!(TuiColor::LightRed, Color::from_str("light red")?.get());
-        assert_eq!(TuiColor::LightBlue, Color::from_str("lightblue")?.get());
-        assert_eq!(
-            TuiColor::Rgb(152, 157, 69),
-            Color::from_str("989D45")?.get()
-        );
-        assert_eq!(TuiColor::Rgb(18, 49, 47), Color::from_str("12312F")?.get());
-        assert_eq!(
-            TuiColor::Rgb(255, 242, 255),
-            Color::from_str("FFF2FF")?.get()
-        );
-        Ok(())
-    }
-    #[test]
     fn test_style() -> Result<()> {
         assert_eq!(
             Colors {
-                bg: Color::from_str("red")?,
-                fg: Color::from_str("blue")?,
+                bg: TuiColor::from_str("red")?,
+                fg: TuiColor::from_str("blue")?,
             },
             Colors::new("red", "blue")?
         );
